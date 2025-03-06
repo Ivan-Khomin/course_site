9b0d09dd38fda7d2fc6d263712466e38c82fa819
Ivan Khomin <homin151210@gmail.com>
2025-03-06 09:13:05 +0200

    Updated

--- courses/models.py
+++ courses/models.py
@@ -45,8 +45,4 @@ class Course(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('courses:course_detail', args=[
-            self.slug,
-            self.id
-        ])
-
+        return reverse('courses:course_detail', args=[self.id])
